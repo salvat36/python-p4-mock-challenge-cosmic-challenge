46776896eed37634765d17a9569942b124a6e5c8
Ryan <salvat36@gmail.com>
2023-06-21 11:17:09 -0500

    wip

--- server/models.py
+++ server/models.py
@@ -24,6 +24,12 @@ class Planet(db.Model, SerializerMixin):
     distance_from_earth = db.Column(db.String)
     nearest_star = db.Column(db.String)
     image = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    missions = db.relationship('Mission', back_populates='planet')
+
+    scientists = association_proxy('missions', 'planet')
 
     def __repr__(self):
         return f'<Planet {self.id}: {self.name}>'
@@ -35,6 +41,25 @@ class Scientist(db.Model, SerializerMixin):
     name = db.Column(db.String)
     field_of_study = db.Column(db.String)
     avatar = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default= db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate= db.func.now())
+
+    missions = db.relationship('Mission', back_populates='scientist')
+
+    planets = association_proxy('missions', 'scientist')
+
+    @validates('name')
+    def validate(self, key, name):
+        if not name or name in [scientist.name for scientist in Scientist.query.all()]:
+            raise ValueError('Must have a name and must be unique')
+        return name
+    
+    @validates('field_of_study')
+    def validate(self, key, field_of_study):
+        if not field_of_study:
+            raise ValueError("must have a field_of_study")
+        return field_of_study
+        
 
     def __repr__(self):
         return f'<Scientist {self.id}: {self.name}>'
@@ -43,5 +68,33 @@ class Mission(db.Model, SerializerMixin):
     __tablename__ = 'missions'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    scientist_id = db.Column(db.Integer, db.ForeignKey('scientists.id'))
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    planet = db.relationship('Planet', back_populates='missions')
+    scientist = db.relationship('Scientist', back_populates='missions')
+
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError('must include a name') 
+        return name
+    
+    @validates('scientist')
+    def validate_scientist(self, key, scientist):
+        if not scientist or scientist in [mission.scientist for mission in Mission.query.all()]:
+            raise ValueError('must include a scientist who has not been on this mission previously') 
+        return scientist
+    
+    @validates('planet')
+    def validate_planet(self, key, planet):
+        if not planet:
+            raise ValueError('must include a planet') 
+        return planet
+    
+
 
 # add any models you may need. 
